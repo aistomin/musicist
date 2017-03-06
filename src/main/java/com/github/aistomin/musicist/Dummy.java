516fb8bc205f759d322e5857c9bf9801304d22b9
aistomin <andrej.istomin.ikeen@gmail.com>
2017-03-06 02:21:18 +0100

    Issue #1. Maven project's skeleton was created.

---
+++ src/main/java/com/github/aistomin/musicist/Dummy.java
@@ -0,0 +1,9 @@
+package com.github.aistomin.musicist;
+
+/**
+ * Created by aistomin on 06/03/2017.
+ *
+ * Dummy class. It was created to commit the folders hierarchy to git.
+ */
+final public class Dummy {
+}
---
+++ src/test/java/com/github/aistomin/musicist/DummyTest.java
@@ -0,0 +1,21 @@
+package com.github.aistomin.musicist;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by aistomin on 06/03/2017.
+ *
+ * Test for {@link Dummy}
+ */
+public class DummyTest {
+
+    /**
+     * Just a dummy test.
+     * @throws Exception If something goes wrong.
+     */
+    @Test
+    public void testDummy() throws Exception {
+        Assert.assertNotNull(new Dummy());
+    }
+}
