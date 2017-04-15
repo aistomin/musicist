3501c9c6324094f0734f7cd1cdb66b0827b19120
aistomin <andrej.istomin.ikeen@gmail.com>
2017-04-15 11:20:23 +0200

    Issue #10. - login with facebook is implemented.

--- src/main/java/com/github/aistomin/musicist/HelloController.java
+++ src/main/java/com/github/aistomin/musicist/HelloController.java
@@ -1,5 +1,12 @@
 package com.github.aistomin.musicist;
 
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by aistomin on 06/03/2017.
- *
+ * <p>
  * Demo Spring MVC controller.
  */
 @Controller
@@ -17,6 +24,7 @@ public final class HelloController {
 
     /**
      * Print welcome message on the "Hello World" page.
+     *
      * @param model The model.
      * @return The welcome message.
      */
@@ -28,16 +36,52 @@ public final class HelloController {
 
     /**
      * Log In.
-     * @param model The model.
-     * @return The welcome message.
+     *
+     * @param request  HTTP request.
+     * @param response HTTP response.
+     * @throws IOException If problem occurs.
      */
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(final ModelMap model) {
-        return "index";
+    @RequestMapping(value = "/facebook/login", method = RequestMethod.GET)
+    public void loginWithFb(
+        final HttpServletRequest request, final HttpServletResponse response
+    ) throws IOException {
+        final Facebook facebook = new FacebookFactory().getInstance();
+        request.getSession().setAttribute("facebook", facebook);
+        final StringBuffer callbackURL = request.getRequestURL();
+        callbackURL.replace(
+            callbackURL.lastIndexOf("/"), callbackURL.length(), ""
+        ).append("/facebook/callback");
+        response.sendRedirect(
+            facebook.getOAuthAuthorizationURL(callbackURL.toString())
+        );
+    }
+
+    /**
+     * Facebook callback processor.
+     *
+     * @param request  HTTP request.
+     * @param response HTTP response.
+     * @throws IOException      If problem occurs.
+     * @throws ServletException If problem occurs.
+     */
+    @RequestMapping(value = "/facebook/callback", method = RequestMethod.GET)
+    public void fbCallback(
+        final HttpServletRequest request, final HttpServletResponse response
+    ) throws IOException, ServletException {
+        final Facebook facebook = (Facebook) request.getSession()
+            .getAttribute("facebook");
+        final String oauthCode = request.getParameter("code");
+        try {
+            facebook.getOAuthAccessToken(oauthCode);
+        } catch (FacebookException e) {
+            throw new ServletException(e);
+        }
+        response.sendRedirect(request.getContextPath() + "/");
     }
 
     /**
      * Renders the "Hello World" page.
+     *
      * @param name The name of the user.
      * @return The model and the view.
      */
--- src/main/webapp/WEB-INF/views/jsp/login.jsp
+++ src/main/webapp/WEB-INF/views/jsp/login.jsp
@@ -12,12 +12,12 @@
                 </div>
             </div><!-- /.modal-header -->
             <div class="modal-body">
-                <a href="#"><img src="${pageContext.request.contextPath}/resources/core/img/social/facebook.png"></a>
-                <a href="#"><img src="${pageContext.request.contextPath}/resources/core/img/social/google.png"></a>
-                <a href="#"><img src="${pageContext.request.contextPath}/resources/core/img/social/twitter.png"></a>
-                <a href="#"><img src="${pageContext.request.contextPath}/resources/core/img/social/github.png"></a>
-                <a href="#"><img src="${pageContext.request.contextPath}/resources/core/img/social/linkedin.png"></a>
-                <a href="#"><img src="${pageContext.request.contextPath}/resources/core/img/social/instagram.png"></a>
+                <a href="${pageContext.request.contextPath}/login"><img src="${pageContext.request.contextPath}/resources/core/img/social/facebook.png"></a>
+                <%--<a href="#"><img src="${pageContext.request.contextPath}/resources/core/img/social/google.png"></a>--%>
+                <%--<a href="#"><img src="${pageContext.request.contextPath}/resources/core/img/social/twitter.png"></a>--%>
+                <%--<a href="#"><img src="${pageContext.request.contextPath}/resources/core/img/social/github.png"></a>--%>
+                <%--<a href="#"><img src="${pageContext.request.contextPath}/resources/core/img/social/linkedin.png"></a>--%>
+                <%--<a href="#"><img src="${pageContext.request.contextPath}/resources/core/img/social/instagram.png"></a>--%>
             </div><!-- /.modal-body -->
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
