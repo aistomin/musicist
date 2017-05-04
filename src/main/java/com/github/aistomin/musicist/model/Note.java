09c86cad31e3353c702218a54c1bb8bccc78ef02
aistomin <andrej.istomin.ikeen@gmail.com>
2017-05-04 15:53:52 +0200

    Issue #12. - the notes are localised.

--- src/main/java/com/github/aistomin/musicist/model/Note.java
+++ src/main/java/com/github/aistomin/musicist/model/Note.java
@@ -10,60 +10,81 @@ public enum Note {
     /**
      * Note C.
      */
-    C,
+    C("note.c"),
 
     /**
      * Note C#.
      */
-    C_SHARP,
+    C_SHARP("note.c.sharp"),
 
     /**
      * Note D.
      */
-    D,
+    D("note.d"),
 
     /**
      * Note D#.
      */
-    D_SHARP,
+    D_SHARP("note.d.sharp"),
 
     /**
      * Note E.
      */
-    E,
+    E("note.e"),
 
     /**
      * Note F.
      */
-    F,
+    F("note.f"),
 
     /**
      * Note F#.
      */
-    F_SHARP,
+    F_SHARP("note.f.sharp"),
 
     /**
      * Note G.
      */
-    G,
+    G("note.g"),
 
     /**
      * Note G#.
      */
-    G_SHARP,
+    G_SHARP("note.g.sharp"),
 
     /**
      * Note A.
      */
-    A,
+    A("note.a"),
 
     /**
      * Note A#.
      */
-    A_SHARP,
+    A_SHARP("note.a.sharp"),
 
     /**
      * Note B.
      */
-    B
+    B("note.b");
+
+    /**
+     * Internationalisation key for displaying the note.
+     */
+    private final String i18nkey;
+
+    /**
+     * Ctor.
+     * @param key Internationalisation key for displaying the note.
+     */
+    Note(final String key) {
+        this.i18nkey = key;
+    }
+
+    /**
+     * Get Internationalisation key for displaying the note.
+     * @return Key.
+     */
+    public String getI18nkey() {
+        return i18nkey;
+    }
 }
