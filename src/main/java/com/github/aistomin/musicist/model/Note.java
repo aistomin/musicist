01cb7afa276a0f1416c1fae4a8eda25f90b0baf0
aistomin <andrej.istomin.ikeen@gmail.com>
2017-05-04 15:30:47 +0200

    Issue #12. - Basic Note.java class was added.

---
+++ src/main/java/com/github/aistomin/musicist/model/Note.java
@@ -0,0 +1,44 @@
+package com.github.aistomin.musicist.model;
+
+/**
+ * Created by aistomin on 04.05.17.
+ * <p>
+ * Notes enum.
+ */
+public enum Note {
+
+    /**
+     * Note C.
+     */
+    C,
+
+    /**
+     * Note D.
+     */
+    D,
+
+    /**
+     * Note E.
+     */
+    E,
+
+    /**
+     * Note F.
+     */
+    F,
+
+    /**
+     * Note G.
+     */
+    G,
+
+    /**
+     * Note A.
+     */
+    A,
+
+    /**
+     * Note B.
+     */
+    B
+}
