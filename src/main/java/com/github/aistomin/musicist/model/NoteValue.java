ee4bed725d75ecd43906129e9c9db497d7d30379
aistomin <andrej.istomin.ikeen@gmail.com>
2017-05-05 14:12:10 +0200

    Issue #12. - note values were added as enum.

---
+++ src/main/java/com/github/aistomin/musicist/model/NoteValue.java
@@ -0,0 +1,41 @@
+package com.github.aistomin.musicist.model;
+
+/**
+ * Created by aistomin on 05.05.17.
+ *
+ * In music notation, a note value indicates the relative duration of a note,
+ * using the color or shape of the note head, the presence or absence of a stem,
+ * and the presence or absence of flags/beams/hooks/tails.
+ */
+public enum NoteValue {
+
+    /**
+     * Whole note.
+     */
+    WHOLE,
+
+    /**
+     * Half note.
+     */
+    HALF,
+
+    /**
+     * Quarter note.
+     */
+    QUARTER,
+
+    /**
+     * Eighth note.
+     */
+    EIGHTH,
+
+    /**
+     * Sixteenth note.
+     */
+    SIXTEENTH,
+
+    /**
+     * Thirty-second note.
+     */
+    THIRTY_SECOND
+}
