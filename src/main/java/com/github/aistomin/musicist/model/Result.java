7e1f95c225bdf27fd52b7f9553284b5ed72f6a89
aistomin <andrej.istomin.ikeen@gmail.com>
2017-05-06 10:39:20 +0200

    Issue #12. - Result.java class draft was created.

---
+++ src/main/java/com/github/aistomin/musicist/model/Result.java
@@ -0,0 +1,12 @@
+package com.github.aistomin.musicist.model;
+
+/**
+ * Created by aistomin on 06.05.17.
+ *
+ * Result of the user for current question. Contains:
+ * 1. User
+ * 2. Question
+ * 3. Points
+ */
+public final class Result {
+}
